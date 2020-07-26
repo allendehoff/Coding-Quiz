2d4003e5457f13b7e40d22c37864dad68fa27a00
Allen DeHoff <allen.dehoff21@gmail.com>
2020-07-26 17:31:47 -0600

    made answer tracker visible after starting quiz

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var startButton = document.getElementById("start")
 var introEl = document.getElementById("intro")
 var questionEl = document.getElementById("question")
+var answerTracker = document.getElementById("answer-tracker")
 
 startButton.addEventListener("click", function(){
     introEl.setAttribute("class", "invisible");
     questionEl.classList.remove("invisible");
+    answerTracker.classList.remove("invisible");
 })
