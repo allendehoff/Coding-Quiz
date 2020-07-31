55e54f24be660bf02418d68f0f6f897d9bf11ba5
Allen DeHoff <allen.dehoff21@gmail.com>
2020-07-31 15:26:15 -0600

    added question objects and timer which runs on clicking start button

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ var startButton = document.getElementById("start")
 var introEl = document.getElementById("intro")
 var questionEl = document.getElementById("question")
 var answerTracker = document.getElementById("answer-tracker")
+var timerEl = document.getElementById("timer")
+var secondsLeft = 30
+
+function runTimer() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "Time Remaining: " + secondsLeft;
+  
+        // if(secondsLeft === 0) {
+        // clearInterval(timerInterval);
+        // sendMessage();
+        // }
+        
+    }, 1000);
+  }
 
 //question object
 var questionOne = {
@@ -12,9 +27,43 @@ var questionOne = {
     answerD: "Wrong D"
 }
 
+var questionTwo = {
+    question: "Question two",
+    answerA: "Right A",
+    answerB: "Wrong B",
+    answerC: "Wrong C",
+    answerD: "Wrong D"
+}
+
+var questionThree = {
+    question: "Question three",
+    answerA: "Wrong A",
+    answerB: "Wrong B",
+    answerC: "Wrong C",
+    answerD: "Right D"
+}
+
+var questionFour = {
+    question: "Question four",
+    answerA: "Wrong A",
+    answerB: "Right B",
+    answerC: "Wrong C",
+    answerD: "Wrong D"
+}
+
+var questionFive = {
+    question: "Question five",
+    answerA: "Right A",
+    answerB: "Wrong B",
+    answerC: "Wrong C",
+    answerD: "Wrong D"
+}
+
 startButton.addEventListener("click", function(){
+    timerEl.textContent = "Time Remaining: 30";
     introEl.setAttribute("class", "hide");
     questionEl.classList.remove("hide");
     answerTracker.classList.remove("hide");
+    runTimer()
 })
 
