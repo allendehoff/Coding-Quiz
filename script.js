56f9ad4f065f4c486734a576f2103cf7da2bd421
Allen DeHoff <allen.dehoff21@gmail.com>
2020-07-31 15:10:13 -0600

    added class of hide instead of hidden to utilize display:none property |

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ var introEl = document.getElementById("intro")
 var questionEl = document.getElementById("question")
 var answerTracker = document.getElementById("answer-tracker")
 
+//question object
+var questionOne = {
+    question: "Question one",
+    answerA: "Wrong A",
+    answerB: "Wrong B",
+    answerC: "Right C",
+    answerD: "Wrong D"
+}
+
 startButton.addEventListener("click", function(){
-    introEl.setAttribute("class", "invisible");
-    questionEl.classList.remove("invisible");
-    answerTracker.classList.remove("invisible");
+    introEl.setAttribute("class", "hide");
+    questionEl.classList.remove("hide");
+    answerTracker.classList.remove("hide");
 })
+
