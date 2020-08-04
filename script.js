62383de571fda45950f58c1868abce4f3d005e88
Allen DeHoff <allen.dehoff21@gmail.com>
2020-08-04 14:17:57 -0600

    changes made with tutor including changing the way the answers are populated, the way a click is handled, and the way certain elements of the HTML are targeted, and added code necessary to iterate to next question on clicking an answer. Old code still present but commented out.

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ var introEl = document.getElementById("intro")
 var questionEl = document.getElementById("questionBlock")
 var answerTracker = document.getElementById("answer-tracker")
 var timerEl = document.getElementById("timer")
-var secondsLeft = 5
+var secondsLeft = 30
 var score = 0
+var timerInterval;
+var questionIndex = 0;
 
 var question = document.getElementById("question")
-var answerEls = [
-    document.getElementById("answer-one"),
-    document.getElementById("answer-two"),
-    document.getElementById("answer-three"),
-    document.getElementById("answer-four")
-]
+// var answerEls = [
+//     document.getElementById("answer-one"),
+//     document.getElementById("answer-two"),
+//     document.getElementById("answer-three"),
+//     document.getElementById("answer-four")
+// ]
 
 var answer = document.getElementsByClassName("answer")
 
@@ -28,87 +30,102 @@ var questions = [
     {
         question: "What color is the sky?",
         answers: ["purple", "yellow", "blue", "red"],
-        checks: ["wrong", "wrong", "right", "wrong"]
+        checks: 2
     },
     {
         question: "Which of these swims?",
         answers: ["fish", "zebra", "elephant", "turkey"],
-        checks: ["right", "wrong", "wrong", "wrong"]
+        checks: 0
     },
     {
         question: "What does the dog say?",
         answers: ["moo", "woof", "meow", "cockadoodledoo"],
-        checks: ["wrong", "right", "wrong", "wrong"]
+        checks: 1
     },
     {
         question: "Who makes the MacBook?",
         answers: ["Acer", "Samsung", "Intel", "Apple"],
-        checks: ["wrong", "wrong", "wrong", "right"]
+        checks: 3
     },
     {
         question: "Which of these is a programming language?",
         answers: ["Spanish", "JavaScript", "Klingon", "Gibberish"],
-        checks : ["wrong", "right", "wrong", "wrong"]
+        checks: 1
     }
 ]
 
 
 
 
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
     timerEl.textContent = "Time Remaining: 30";
     introEl.classList.add("hide");
     questionEl.classList.remove("hide");
     answerTracker.classList.remove("hide");
     runTimer()
-    renderQuestion(4)
+    renderQuestion(questionIndex)
 })
 
 function runTimer() {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = "Time Remaining: " + secondsLeft;
 
-        if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        endQuiz();
+        if (secondsLeft === 0) {
+            endQuiz();
         }
-        
+
     }, 1000);
 }
 
-function endQuiz(){
+function endQuiz() {
+    clearInterval(timerInterval);
     timerEl.classList.add("hide")
     storeScore()
 }
 
-
-
-
-function renderQuestion(x){
+function renderQuestion(x) {
     question.textContent = questions[x].question;
-    answerEls[0].textContent = questions[x].answers[0];
-    answerEls[0].setAttribute("data-check", questions[x].checks[0]);
-    answerEls[1].textContent = questions[x].answers[1];
-    answerEls[1].setAttribute("data-check", questions[x].checks[1]);
-    answerEls[2].textContent = questions[x].answers[2];
-    answerEls[2].setAttribute("data-check",questions[x].checks[2]);
-    answerEls[3].textContent = questions[x].answers[3];
-    answerEls[3].setAttribute("data-check", questions[x].checks[3]);
 
-    for (const ans of answer){
-        ans.addEventListener("click", function(){
-            if (ans.getAttribute("data-check") === "right"){
-                tracker[x].classList.add("right")
-                score++
-            }
-            else if (ans.getAttribute("data-check") === "wrong"){
-                tracker[x].classList.add("wrong")
-                score --
-            }
-        })
-        }
+    for (var i = 0; i < answer.length; i++) {
+        answer[i].textContent = questions[x].answers[i]
+        answer[i].addEventListener('click', handleClick)
+    }
+    // answerEls[0].textContent = questions[x].answers[0];
+    // // answerEls[0].setAttribute("data-check", questions[x].checks[0]);
+    // answerEls[1].textContent = questions[x].answers[1];
+    // // answerEls[1].setAttribute("data-check", questions[x].checks[1]);
+    // answerEls[2].textContent = questions[x].answers[2];
+    // // answerEls[2].setAttribute("data-check", questions[x].checks[2]);
+    // answerEls[3].textContent = questions[x].answers[3];
+    // // answerEls[3].setAttribute("data-check", questions[x].checks[3]);
+
+    // for (const ans of answer) {
+    //     ans.addEventListener("click", function () {
+    //         if (ans.getAttribute("data-check") === "right") {
+    //             tracker[x].classList.add("right")
+    //             score++
+    //         }
+    //         else if (ans.getAttribute("data-check") === "wrong") {
+    //             tracker[x].classList.add("wrong")
+    //             score--
+    //         }
+    //     })
+    // }
 }
+
+function handleClick() {
+    var guessed = questions[questionIndex].answers.indexOf(this.textContent)
+    var correct = questions[questionIndex].checks
+    console.log(guessed === correct)
+
+
+    questionIndex++;
+    if (questionIndex < questions.length) {
+        renderQuestion(questionIndex)
+    }
+}
+
 
 var highScores = []
 var jumbo = document.getElementById("scoreboard")
@@ -118,9 +135,9 @@ var modal = document.getElementById("score-modal")
 var playerName = document.getElementById("player-name")
 var submitBtn = document.getElementById("submit-score")
 
-function storeScore(){
+function storeScore() {
     modal.classList.add("show")
-    submitBtn.addEventListener("click", function(){
+    submitBtn.addEventListener("click", function () {
         event.preventDefault()
         var newBreak = document.creatElement("hr")
         var newPlayer = jumbo.creatElement("p")
@@ -132,17 +149,17 @@ function storeScore(){
     })
 }
 
-function toggleScoreboard(){
-    if (jumbo.className === hide){
+function toggleScoreboard() {
+    if (jumbo.className === hide) {
         jumbo.classList.remove("hide")
     }
-    else{
+    else {
         jumbo.classList.add("hide")
     }
 }
 
-clearBtn.addEventListener("click", function(){
-    while(leaders.firstChild) leaders.removeChild(leaders.firstChild)
+clearBtn.addEventListener("click", function () {
+    while (leaders.firstChild) leaders.removeChild(leaders.firstChild)
 })
 
 
@@ -192,7 +209,7 @@ clearBtn.addEventListener("click", function(){
 //                 tracker[i].classList.add("wrong")
 //                 score --
 //             }
-    
+
 //             console.log(score)
 //         })
 //         }
